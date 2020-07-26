8f9107e031a329c146c9e037b51ca89e4ae83472
Nghia Phan <nghiaphan1407@gmail.com>
2020-07-26 15:44:20 +0700

    Updates

---
+++ precache-manifest.1f722da1d0ac32bb00e1818c5c7482e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08a6d9117a02aceab65e9952ef9ebb2",
+    "url": "/personal-blog/index.html"
+  },
+  {
+    "revision": "b70da60e1af9cb4358de",
+    "url": "/personal-blog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c356a0772cd395915d3c",
+    "url": "/personal-blog/static/css/main.4bcf9192.chunk.css"
+  },
+  {
+    "revision": "b70da60e1af9cb4358de",
+    "url": "/personal-blog/static/js/2.7cb24997.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-blog/static/js/2.7cb24997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c356a0772cd395915d3c",
+    "url": "/personal-blog/static/js/main.810496bf.chunk.js"
+  },
+  {
+    "revision": "d863a11c94f647acc9f1",
+    "url": "/personal-blog/static/js/runtime-main.a9cc2822.js"
+  },
+  {
+    "revision": "7fdff8df1e51772e1d5ff3e8cc641171",
+    "url": "/personal-blog/static/media/self.7fdff8df.png"
+  }
+]);
