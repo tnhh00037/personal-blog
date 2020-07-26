b29b4b2148166d3b4dc72aad8a890d1582843519
Nghia Phan <nghiaphan1407@gmail.com>
2020-07-26 15:30:54 +0700

    feat: add Main page

---
+++ src/Components/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default function MainBody() {
+     return (
+          <div className="container content">
+              <h1>
+                  HOME
+              </h1>
+          </div>
+     )
+}
