b2b5b05bebdcc7b49b475d6e88a68b82a33182db
Nghia Phan <nghiaphan1407@gmail.com>
2020-07-26 15:31:06 +0700

    chore: add index to export child

---
+++ src/Components/index.tsx
@@ -0,0 +1,7 @@
+export {default as Sidebar} from './Sidebar'
+export {default as Header} from './Header'
+export {default as Footer} from './Footer'
+export {default as Main} from './Main'
+export {default as Article} from './Articles'
+export {default as AboutMe} from './About'
+
